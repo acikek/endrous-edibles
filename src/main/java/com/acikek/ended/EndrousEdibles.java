b16ec26b6ea961b49f37f297cf1d5fc8d8c2c5a1
acikek <spiralixp@gmail.com>
2023-01-26 16:39:58 -0800

    remove debug edibles

--- src/main/java/com/acikek/ended/EndrousEdibles.java
+++ src/main/java/com/acikek/ended/EndrousEdibles.java
@@ -1,17 +1,9 @@
 package com.acikek.ended;
 
-import com.acikek.ended.api.EndrousEdiblesAPI;
-import com.acikek.ended.api.builder.DestinationBuilder;
-import com.acikek.ended.api.builder.EdibleBuilder;
-import com.acikek.ended.api.builder.RuleBuilder;
 import com.acikek.ended.command.EdibleCommand;
-import com.acikek.ended.edible.Edible;
 import com.acikek.ended.load.EdibleLoader;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Items;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,19 +19,6 @@ public class EndrousEdibles implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Edible edible = EdibleBuilder.create()
-                .edible(Ingredient.ofItems(Items.MELON_SLICE))
-                .addRule(RuleBuilder.create()
-                        .addSource(new Identifier("minecraft:overworld"), false)
-                        .addDestination("spawn", DestinationBuilder.create()
-                                .world(World.OVERWORLD)
-                                .worldSpawn()
-                                .build())
-                        .build())
-                .build(new Identifier("example:overworld_melon"));
-        System.out.println(edible);
-        EndrousEdiblesAPI.registerEdible(edible);
-
         LOGGER.info("Thanks for getting a taste of Endrous Edibles!");
         EdibleLoader.register();
         EdibleCommand.register();
